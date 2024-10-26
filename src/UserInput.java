d43eff97a8d72225a0d07f560de11c7ce9b96762
ShubhamPokhariyal <shubhampokhariyal2@gmail.com>
2024-10-26 11:45:20 +0530

    updated userinput.java

--- src/UserInput.java
+++ src/UserInput.java
@@ -5,7 +5,16 @@ public class UserInput {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
-        System.out.println(name);
+        System.out.printf("hello %s. How old are you?",name);
+        //int age = scanner.nextInt();
+        //clean the buffer
+        //scanner.nextLine();
+        //typecast to int workaround
+        int age = Integer.parseInt(scanner.nextLine());
+        //double gpa = Double.parseDouble(scanner.nextLine());
+        System.out.printf("%d is an excellent age for programming. What is your programming language?",age);
+        String language = scanner.nextLine();
+        System.out.printf("%s is a good language choice.",language);
 
         scanner.close();
     }
