394cf8d5392714d5211ab346afe6f20dace6ad40
ShubhamPokhariyal <shubhampokhariyal2@gmail.com>
2024-11-05 12:34:42 +0530

    added WhileLoop.java

---
+++ src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int number = 5;
+        int multiplier = 1;
+        while(multiplier <= 10){
+            System.out.printf("%d X %d = %d\n",number,multiplier,number*multiplier);
+            multiplier++;
+        }
+    }
+}
